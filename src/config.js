a711d1661e10c46a97384b64d762a2654c63ed43
Shukhratovich-a <shukhratovich138@gmail.com>
2022-06-18 03:42:29 +0500

    Edit host name

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const PORT = process.env.PORT || 6500;
-const HOST = "http://localhost:" + PORT;
+// const HOST = "http://localhost:" + PORT;
+const HOST = "https://epic-chat-back.herokuapp.com";
 
 export { HOST, PORT };
